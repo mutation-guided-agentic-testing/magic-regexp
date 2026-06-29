b7e37f696959c3379b693d8d53e5e2afb71ba1a9
Bram de Vries <acdevries@home.nl>
2026-06-29 10:13:27 +0200

    Remove fields from stryker

--- stryker.config.mjs
+++ stryker.config.mjs
@@ -2,10 +2,7 @@
 /** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
 const config = {
   packageManager: 'pnpm',
-  reporters: ['html', 'clear-text', 'progress', 'dashboard', 'json'],
   testRunner: 'vitest',
-  coverageAnalysis: 'perTest',
-  buildCommand: 'tsc --noEmit',
   plugins: [
     '@stryker-mutator/vitest-runner',
     '@stryker-mutator/typescript-checker',
@@ -13,15 +10,10 @@ const config = {
   // Additions
   mutate: ['src/**/*.ts', '!src/**/*.d.ts'],
   testFiles: ['test/**/*.test.ts'],
-  ignoreStatic: true,
-  jsonReporter: {
-    fileName: '../reports/magix-regexp/stryker-mutations.json',
-  },
   checkers: ['typescript'],
   tsconfigFile: 'tsconfig.json',
   typescriptChecker: {
     prioritizePerformanceOverAccuracy: true,
   },
-  incremental: true,
 }
 export default config
